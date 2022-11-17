66b9eb4a8196ac0b22c56c83730e76a4440a118c
ShimiyonS <112162811+ShimiyonS@users.noreply.github.com>
2022-11-17 23:57:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+var h1=document.getElementById("task");           //created an h1 tag with the id named task using DOM
+h1.innerText="Countdown Starts";                  //inserted a text in h1 tag
+addEventListener("load",()=>{                     //on load is given which loads every other files and finally displays the countdown from 10 to 1 and displays happy independence day
+    setTimeout(()=>{
+        h1.innerText="10"
+    setTimeout(()=>{
+        h1.innerText="9"
+    setTimeout(()=>{
+        h1.innerText="8"
+    setTimeout(()=>{
+        h1.innerText="7"
+    setTimeout(()=>{
+        h1.innerText="6" 
+    setTimeout(()=>{
+        h1.innerText="5"
+    setTimeout(()=>{
+        h1.innerText="4"
+    setTimeout(()=>{
+        h1.innerText="3"
+    setTimeout(()=>{
+        h1.innerText="2"
+    setTimeout(()=>{
+        h1.innerText="1"
+    setTimeout(()=>{
+        h1.innerText="Happy Independence Day!!!"
+    },2000)        
+    },1000)                                
+    },1000)     
+    },1000)      
+    },1000)  
+    },1000)     
+    },1000)
+    },1000)
+    },1000)
+    },1000)
+    },1000)                            // The second parameter in the setTimeout function displays in browser page after each seconds.
+})
